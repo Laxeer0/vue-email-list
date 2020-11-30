4f538761c08f165085b993c94fbc8783394d4382
Giuseppe <laxeer0@gmail.com>
2020-11-30 17:17:49 +0100

    End Commit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 var root = new Vue({
     el: '#root',
     data: {
-        
+        apiUrl: "https://flynn.boolean.careers/exercises/api/random/mail",
+        email: [],
+
     },
-    methods: {
+    mounted() {
+       for(var i = 0; i < 10; i++){
+        axios({url: this.apiUrl})
+        .then((response) => {
+            this.email.push(response.data.response);
+        });
+    }
     },
 
 });
